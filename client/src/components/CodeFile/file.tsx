d54cad4f0322433886212a7ca1c7d63477850238
unknown <aliopdandan@gmail.com>
2025-08-22 13:29:36 +0100

    f

--- client/src/components/CodeFile/file.tsx
+++ client/src/components/CodeFile/file.tsx
@@ -7,18 +7,23 @@ export function File() {
   return (
     <div className="flex flex-col h-screen">
       <div className="bg-black text-white px-6 py-4 flex items-center justify-between shadow-md">
-        <h1 className="text-xl font-semibold">Name of the Room</h1>
+        <h1 className="text-lg font-semibold tracking-wide">Name of the Room</h1>
+        <button
+          className="bg-green-500 hover:bg-green-600 active:bg-green-700 text-white font-medium px-4 py-2 rounded-lg shadow-md transition-all duration-200 cursor-pointer"
+        >
+          Run
+        </button>
       </div>
-      {/* Monaco Editor */}
       <div className="flex-grow">
       <Editor
       height="100%"
-      width="50%"
+      width="100%"
       language="javascript"
       theme="vs-dark"
       defaultValue="// Start coding here..."
       />
       </div>
+      <Output/>
     </div>
   );
 }
--- client/src/components/CodeFile/output.tsx
+++ client/src/components/CodeFile/output.tsx
@@ -1,24 +1,11 @@
 import React from "react";
-import { Box, Button, Text } from "@chakra-ui/react";
 
-export function Output() {
-  return (
-    <Box w="50%">
-      <Text mb={2} fontSize="lg">
-        Output
-      </Text>
-      <Button variant="outline" colorScheme="green" mb={4}>
-        Run Code
-      </Button>
-      <Box
-        height="75vh"
-        p={2}
-        border="1px solid"
-        borderRadius={4}
-        borderColor="#333"
-      >
-        Click "Run Code" to see the output here
-      </Box>
-    </Box>
-  );
+export function Output(){
+    return (
+        <div className="p-4">
+            <div className="bg-black text-green-400 font-mono p-2 h-40 rounded overflow-y-auto">
+            Your code is here
+            </div>
+        </div>
+    )
 }
