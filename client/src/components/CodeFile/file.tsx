61864ab969e2befdfa1f7a7c271c96b7bb0f83e6
unknown <aliopdandan@gmail.com>
2025-08-31 13:01:54 +0100

    f

--- client/src/components/CodeFile/file.tsx
+++ client/src/components/CodeFile/file.tsx
@@ -37,7 +37,7 @@ export function File() {
   return (
     <div className="flex flex-col h-screen">
       <div className="bg-black text-white px-6 py-4 flex items-center justify-between shadow-md">
-        <h1 className="text-lg font-semibold tracking-wide">Name of the Room</h1>
+        <h1 className="text-lg font-semibold tracking-wide">DevCollab</h1>
         <button
           className="bg-green-500 hover:bg-green-600 active:bg-green-700 text-white font-medium px-4 py-2 rounded-lg shadow-md transition-all duration-200 cursor-pointer"
           onClick={runCode}
--- client/src/components/CodeFile/output.tsx
+++ client/src/components/CodeFile/output.tsx
@@ -4,6 +4,7 @@ export function Output({ output }: { output: string }){
     return (
         <div className="p-4">
             <div className="bg-black text-green-400 font-mono p-2 h-40 rounded overflow-y-auto">
+                Hello world
                 {output}
             </div>
         </div>
