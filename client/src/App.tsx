bf787e3aa6e566a38fd07c903320a88ad56a0d2a
unknown <aliopdandan@gmail.com>
2025-09-03 08:55:17 +0100

    f

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 import { File } from "./components/CodeFile/file"
+import { Login } from "./components/auth/login"
+import { Signup } from "./components/auth/signup"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <File/>
+    <Router>
+      <Routes>
+        <Route element={<Login/>} path="/login"/>
+        <Route element={<Signup/>} path="/signup"/>
+        <Route element={<File/>} path="/"/>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ client/src/components/auth/login.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+
+export function Login(){
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+
+    function handleSubmit(){
+        return null
+    }
+
+     return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-200">
+            <div className="max-w-md w-full p-6 bg-white rounded-lg shadow-md">
+                <h1 className="text-2xl font-semibold mb-6 text-center">Log in</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label htmlFor="username" className="block text-gray-700 font-bold mb-2">Username</label>
+                        <input
+                            type="text"
+                            id="username"
+                            name="username"
+                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="password" className="block text-gray-700 font-bold mb-2">Password</label>
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-blue-500"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <div className="mb-4 text-red-500"></div>
+                    <div className="mb-6">
+                        <button type="submit" className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Log In</button>
+                    </div>
+                </form>
+                <a href="/sign-up">Create an account</a>
+            </div>
+        </div>
+    );
+}
